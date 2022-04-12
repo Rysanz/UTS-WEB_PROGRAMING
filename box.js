5067aad3ae0c3f696d0eaaad12080f3258a7fdcb
Rysanz <aryganteng13@gmail.com>
2022-04-12 17:19:16 +0800

    nyoba commit ulang

--- box.js
+++ box.js
@@ -5,4 +5,10 @@ document.onmousemove = (event) => {
     var y = event.clientY;
     box1.style.left = x - (box1.clientWidth/2) + "px";
     box1.style.top = y - (box1.clientWidth/2) + "px";
+
 }
+
+function random(number) {
+    return Math.floor(Math.random()* number)
+}
+
